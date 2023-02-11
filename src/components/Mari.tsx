807d56be0c9ae082c7754310a111c57d66a6fa0f
Kev <sulpeum@gmail.com>
2023-02-11 16:09:19 -0500

    test

--- src/components/Mari.tsx
+++ src/components/Mari.tsx
@@ -242,11 +242,7 @@ export const Mari = () => {
             </tr>
           </thead>
 
-          {isLoading ? (
-            <div className="grid place-items-center py-14">
-              One moment while we fetch the data...
-            </div>
-          ) : localDataAPI && finalData && !error ? (
+          {localDataAPI && finalData && !error ? (
             <tbody className="text-[0.825rem] tracking-tighter ">
               {finalData.map(
                 ({
