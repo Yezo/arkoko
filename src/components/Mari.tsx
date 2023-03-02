2eb15a558dfbf94cfbeeb0a9bcd9318dafd0d1f3
Kev <sulpeum@gmail.com>
2023-03-01 23:00:32 -0500

    fixed table header padding

--- src/components/Mari.tsx
+++ src/components/Mari.tsx
@@ -1,166 +1,161 @@
-import { useState, useEffect } from "react";
-import { Dropdown } from "./Dropdown";
-import { TableNav } from "./TableNav";
-import { TableRow } from "./TableRow";
+import { useState, useEffect } from "react"
+import { Dropdown } from "./Dropdown"
+import { TableNav } from "./TableNav"
+import { TableRow } from "./TableRow"
 
 type marketAPI = {
-  name: string;
-  quantity: number;
-  id: string;
-  gameCode: number | string;
-  image: string;
-  lowPrice: number;
-};
+  name: string
+  quantity: number
+  id: string
+  gameCode: number | string
+  image: string
+  lowPrice: number
+}
 
 type localAPI = {
-  name: string;
-  bluecrystal: number;
-  quantity: number;
-};
+  name: string
+  bluecrystal: number
+  quantity: number
+}
 type dataAPI = {
-  name: string;
-  bluecrystal: number;
-  quantity: number;
-  id: string;
-  gameCode: number | string;
-  image: string;
-  lowPrice: number;
-  total: number;
-};
+  name: string
+  bluecrystal: number
+  quantity: number
+  id: string
+  gameCode: number | string
+  image: string
+  lowPrice: number
+  total: number
+}
 
 export const Mari = () => {
   //States
-  const [region, setRegion] = useState("North America East");
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [error, setError] = useState<boolean>(false);
-  const [marketDataAPI, setMarketDataAPI] = useState<marketAPI[] | null>([]);
-  const [localDataAPI, setLocalDataAPI] = useState<localAPI[] | null>([]);
-  const [finalData, setFinalData] = useState<dataAPI[] | null>([]);
-  const [crystal, setCrystal] = useState(0);
+  const [region, setRegion] = useState("North America East")
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [error, setError] = useState<boolean>(false)
+  const [marketDataAPI, setMarketDataAPI] = useState<marketAPI[] | null>([])
+  const [localDataAPI, setLocalDataAPI] = useState<localAPI[] | null>([])
+  const [finalData, setFinalData] = useState<dataAPI[] | null>([])
+  const [crystal, setCrystal] = useState(0)
   const [sorting, setSorting] = useState({
     key: "name",
     ascending: true,
-  });
+  })
 
   /* -------------------------------------------------------------------------- */
   /*                                  Constants                                 */
   /* -------------------------------------------------------------------------- */
-  const gold = "https://www.lostarkmarket.online/assets/icons/gold.png";
-  const blue = "https://www.lostarkmarket.online/assets/icons/bluecrystal.png";
-  const localApiURL = `https://arkoko-api.onrender.com/api/marishop`;
-  const blueCrystalURL = `https://www.lostarkmarket.online/api/export-market-live/${region}?category=Currency Exchange`;
-  const apiURL = `https://www.lostarkmarket.online/api/export-market-live/${region}?items=honor-shard-pouch-l-3,honor-shard-pouch-s-1,great-honor-leapstone-2,honor-leapstone-2,solar-grace-1,solar-blessing-2,solar-protection-3,basic-oreha-fusion-material-2,simple-oreha-fusion-material-1,superior-oreha-fusion-material-4,crystallized-destruction-stone-0,crystallized-guardian-stone-0,marvelous-honor-leapstone-3`;
+  const gold = "https://www.lostarkmarket.online/assets/icons/gold.png"
+  const blue = "https://www.lostarkmarket.online/assets/icons/bluecrystal.png"
+  const localApiURL = `https://arkoko-api.onrender.com/api/marishop`
+  const blueCrystalURL = `https://www.lostarkmarket.online/api/export-market-live/${region}?category=Currency Exchange`
+  const apiURL = `https://www.lostarkmarket.online/api/export-market-live/${region}?items=honor-shard-pouch-l-3,honor-shard-pouch-s-1,great-honor-leapstone-2,honor-leapstone-2,solar-grace-1,solar-blessing-2,solar-protection-3,basic-oreha-fusion-material-2,simple-oreha-fusion-material-1,superior-oreha-fusion-material-4,crystallized-destruction-stone-0,crystallized-guardian-stone-0,marvelous-honor-leapstone-3`
 
   /* -------------------------------------------------------------------------- */
   /*                              Helper functions                              */
   /* -------------------------------------------------------------------------- */
 
   function generateCrystal(value: number) {
-    return Math.round(value * 95);
+    return Math.round(value * 95)
   }
 
   function convertBlueCrystalToGold(amount: number) {
-    return Math.ceil((amount / 95) * crystal);
+    return Math.ceil((amount / 95) * crystal)
   }
 
   //Sort an array by its game code
   const handleSortArray = (arr: marketAPI[]) =>
-    arr.sort(
-      (a: { gameCode: any }, b: { gameCode: any }) => a.gameCode - b.gameCode
-    );
+    arr.sort((a: { gameCode: any }, b: { gameCode: any }) => a.gameCode - b.gameCode)
 
   //Merging two objects
   const mergeById = (localData, externalData) =>
     externalData.map((itm: { name: any }) => ({
-      ...localData.find(
-        (item: { name: any }) => item.name === itm.name && item
-      ),
+      ...localData.find((item: { name: any }) => item.name === itm.name && item),
       ...itm,
-    }));
+    }))
 
   //Determine if you should buy from the Mari's Shop or from the Marketplace
   const compareBothValues = (a: number, b: number) => {
-    return a >= b ? "MARKET" : "MARI";
-  };
+    return a >= b ? "MARKET" : "MARI"
+  }
 
   useEffect(() => {
-    const controller = new AbortController();
+    const controller = new AbortController()
     const fetchAPI = async () => {
-      setError(false);
-      setIsLoading(true);
+      setError(false)
+      setIsLoading(true)
       try {
         //Fetching the current value of Blue Crystals
-        const resBlueCrystal = await fetch(blueCrystalURL);
-        const dataBlueCrystal = await resBlueCrystal.json();
-        setCrystal(generateCrystal(dataBlueCrystal[0].lowPrice));
+        const resBlueCrystal = await fetch(blueCrystalURL)
+        const dataBlueCrystal = await resBlueCrystal.json()
+        setCrystal(generateCrystal(dataBlueCrystal[0].lowPrice))
 
         //Fetching item data from the external API
-        const data = await fetch(apiURL);
-        const jsonData = await data.json();
-        setMarketDataAPI(handleSortArray(jsonData));
+        const data = await fetch(apiURL)
+        const jsonData = await data.json()
+        setMarketDataAPI(handleSortArray(jsonData))
 
         //Fetching item data from our local API
-        const resLocalAPI = await fetch(localApiURL);
-        const dataLocalAPI = await resLocalAPI.json();
-        setLocalDataAPI(dataLocalAPI);
+        const resLocalAPI = await fetch(localApiURL)
+        const dataLocalAPI = await resLocalAPI.json()
+        setLocalDataAPI(dataLocalAPI)
       } catch (error) {
-        controller.signal.aborted ? console.log("Aborted") : setError(true);
+        controller.signal.aborted ? console.log("Aborted") : setError(true)
       } finally {
-        setIsLoading(false);
+        setIsLoading(false)
       }
-    };
-    fetchAPI();
+    }
+    fetchAPI()
     return () => {
-      controller.abort();
-    };
-  }, [region]);
+      controller.abort()
+    }
+  }, [region])
 
   // Preventing infinite loops;
   useEffect(() => {
     //Merge local and external API data together into one array
-    const tempData = mergeById(localDataAPI, marketDataAPI);
+    const tempData = mergeById(localDataAPI, marketDataAPI)
 
     //Adding new key value pairs to existing data
-    const finale = tempData.map(
-      (item: { quantity: number; lowPrice: number }) =>
-        Object.assign(item, { total: item.quantity * item.lowPrice })
-    );
+    const finale = tempData.map((item: { quantity: number; lowPrice: number }) =>
+      Object.assign(item, { total: item.quantity * item.lowPrice })
+    )
 
-    setFinalData(finale);
-  }, [marketDataAPI, localDataAPI]);
+    setFinalData(finale)
+  }, [marketDataAPI, localDataAPI])
 
   // Tablesort
   useEffect(() => {
     if (finalData) {
-      const currentData = [...finalData];
+      const currentData = [...finalData]
 
       const sortedHeaders = currentData.sort((a, b) => {
-        let fa = a[sorting.key];
-        let fb = b[sorting.key];
+        let fa = a[sorting.key]
+        let fb = b[sorting.key]
 
         if (typeof fa === "string") {
           if (fa < fb) {
-            return -1;
+            return -1
           }
           if (fa > fb) {
-            return 1;
+            return 1
           }
-          return 0;
+          return 0
         }
         if (typeof fa === "number") {
-          return a[sorting.key] - b[sorting.key];
-        } else return 0;
-      });
+          return a[sorting.key] - b[sorting.key]
+        } else return 0
+      })
 
-      setFinalData(sorting.ascending ? sortedHeaders : sortedHeaders.reverse());
+      setFinalData(sorting.ascending ? sortedHeaders : sortedHeaders.reverse())
     }
-  }, [sorting.ascending, sorting.key]);
+  }, [sorting.ascending, sorting.key])
 
   function applySorting(key: any, ascending: any) {
     setSorting({
       key: key,
       ascending: ascending,
-    });
+    })
   }
 
   return (
@@ -207,124 +202,104 @@ export const Mari = () => {
               <thead className="cursor-pointer border-b-[1px] border-[#2B313A]">
                 <tr className="">
                   <th
-                    className="min-w-[10rem] pb-3 pl-2 text-left md:min-w-[20rem]"
+                    className="min-w-[10rem] pb-3 text-left md:min-w-[20rem] md:px-3"
                     onClick={() => {
-                      applySorting("name", !sorting.ascending);
+                      applySorting("name", !sorting.ascending)
                     }}
                   >
                     Name
                   </th>
                   <th
-                    className="table-header pb-3 text-right"
-                    onClick={() =>
-                      applySorting("bluecrystal", !sorting.ascending)
-                    }
+                    className="table-header pb-3 text-right md:px-3"
+                    onClick={() => applySorting("bluecrystal", !sorting.ascending)}
                   >
                     Price
                   </th>
                   <th
-                    className="table-header pb-3 text-right"
-                    onClick={() =>
-                      applySorting("bluecrystal", !sorting.ascending)
-                    }
+                    className="table-header pb-3 text-right md:px-3"
+                    onClick={() => applySorting("bluecrystal", !sorting.ascending)}
                   >
                     Crystal Value
                   </th>
                   <th
-                    className="table-header pb-3 text-right"
+                    className="table-header pb-3 text-right md:px-3"
                     onClick={() => applySorting("total", !sorting.ascending)}
                   >
                     Market Value
                   </th>
                   <th
-                    className="table-header pb-3 text-center"
-                    onClick={() =>
-                      applySorting("bluecrystal", !sorting.ascending)
-                    }
+                    className="table-header pb-3 text-center md:px-3"
+                    onClick={() => applySorting("bluecrystal", !sorting.ascending)}
                   >
                     Buy
                   </th>
                 </tr>
               </thead>
               <tbody className="text-[0.825rem] tracking-tighter ">
-                {finalData.map(
-                  ({
-                    name,
-                    id,
-                    image,
-                    bluecrystal,
-                    quantity,
-                    lowPrice,
-                    total,
-                  }) => {
-                    return (
-                      <tr key={id} className="p-5 hover:bg-primary">
-                        {/* Item name */}
-                        <TableRow position="justify-start">
-                          <span className="max-w-[35px]">
-                            <img src={image} alt={name} />
-                          </span>
-                          <span className="flex items-center gap-1">
-                            {name}
-                            <span className="text-[0.7rem] text-gray-600 ">
-                              x{quantity}
-                            </span>
-                          </span>
-                        </TableRow>
+                {finalData.map(({ name, id, image, bluecrystal, quantity, lowPrice, total }) => {
+                  return (
+                    <tr key={id} className="p-5 hover:bg-primary">
+                      {/* Item name */}
+                      <TableRow position="justify-start">
+                        <span className="max-w-[35px]">
+                          <img src={image} alt={name} />
+                        </span>
+                        <span className="flex items-center gap-1">
+                          {name}
+                          <span className="text-[0.7rem] text-gray-600 ">x{quantity}</span>
+                        </span>
+                      </TableRow>
 
-                        {/* Price */}
-                        <TableRow position="justify-end">
-                          <span className="font-numbers text-[0.9rem] font-medium">
-                            {bluecrystal}
-                          </span>
-                          <img src={blue} alt="blue" className="max-w-[20px]" />
-                        </TableRow>
+                      {/* Price */}
+                      <TableRow position="justify-end">
+                        <span className="font-numbers text-[0.9rem] font-medium">
+                          {bluecrystal}
+                        </span>
+                        <img src={blue} alt="blue" className="max-w-[20px]" />
+                      </TableRow>
 
-                        {/* Crystal Value */}
-                        <TableRow position="justify-end">
-                          <span className="font-numbers text-[0.9rem] font-medium">
-                            {new Intl.NumberFormat().format(
-                              convertBlueCrystalToGold(bluecrystal)
-                            )}
-                          </span>
-                          <img src={gold} alt="gold" className="max-w-[25px]" />
-                        </TableRow>
+                      {/* Crystal Value */}
+                      <TableRow position="justify-end">
+                        <span className="font-numbers text-[0.9rem] font-medium">
+                          {new Intl.NumberFormat().format(convertBlueCrystalToGold(bluecrystal))}
+                        </span>
+                        <img src={gold} alt="gold" className="max-w-[25px]" />
+                      </TableRow>
 
-                        {/* Market Price */}
-                        <TableRow position="justify-end">
-                          <span className="font-numbers text-[0.9rem] font-medium">
-                            {new Intl.NumberFormat().format(total)}
-                          </span>
-                          <img src={gold} alt="gold" className="max-w-[25px]" />
-                        </TableRow>
+                      {/* Market Price */}
+                      <TableRow position="justify-end">
+                        <span className="font-numbers text-[0.9rem] font-medium">
+                          {new Intl.NumberFormat().format(total)}
+                        </span>
+                        <img src={gold} alt="gold" className="max-w-[25px]" />
+                      </TableRow>
 
-                        {/* Buy from Mari or Market */}
-                        <TableRow position="justify-center">
-                          <button
-                            className={`min-w-[5.5rem] rounded bg-primary px-3 py-1 font-bold ring-1 ring-black/[.25] ${
-                              compareBothValues(
-                                convertBlueCrystalToGold(bluecrystal),
-                                lowPrice * quantity
-                              ) === "MARKET"
-                                ? "bg-green-800"
-                                : "bg-red-900"
-                            }`}
-                          >
-                            {compareBothValues(
+                      {/* Buy from Mari or Market */}
+                      <TableRow position="justify-center">
+                        <button
+                          className={`min-w-[5.5rem] rounded bg-primary px-3 py-1 font-bold ring-1 ring-black/[.25] ${
+                            compareBothValues(
                               convertBlueCrystalToGold(bluecrystal),
                               lowPrice * quantity
-                            )}
-                          </button>
-                        </TableRow>
-                      </tr>
-                    );
-                  }
-                )}
+                            ) === "MARKET"
+                              ? "bg-green-800"
+                              : "bg-red-900"
+                          }`}
+                        >
+                          {compareBothValues(
+                            convertBlueCrystalToGold(bluecrystal),
+                            lowPrice * quantity
+                          )}
+                        </button>
+                      </TableRow>
+                    </tr>
+                  )
+                })}
               </tbody>
             </table>
           ) : null}
         </>
       )}
     </div>
-  );
-};
+  )
+}
--- src/components/Market.tsx
+++ src/components/Market.tsx
@@ -1,114 +1,112 @@
-import { useState, useEffect } from "react";
-import { Dropdown } from "./Dropdown";
-import { Sparklines, SparklinesLine } from "react-sparklines";
-import { TableNav } from "./TableNav";
-import { TableRow } from "./TableRow";
+import { useState, useEffect } from "react"
+import { Dropdown } from "./Dropdown"
+import { Sparklines, SparklinesLine } from "react-sparklines"
+import { TableNav } from "./TableNav"
+import { TableRow } from "./TableRow"
 
 /* -------------------------------------------------------------------------- */
 /*                                    TYPES                                   */
 /* -------------------------------------------------------------------------- */
 type marketAPI = {
-  name: string;
-  quantity: number;
-  id: string;
-  gameCode: number | string;
-  image: string;
-  lowPrice: number;
-  cheapestRemaining: number;
-  recentPrice: number;
-  shortHistoric: unknown;
-};
+  name: string
+  quantity: number
+  id: string
+  gameCode: number | string
+  image: string
+  lowPrice: number
+  cheapestRemaining: number
+  recentPrice: number
+  shortHistoric: unknown
+}
 
 export const Market = () => {
   /* -------------------------------------------------------------------------- */
   /*                                   STATES                                   */
   /* -------------------------------------------------------------------------- */
-  const [data, setData] = useState<marketAPI[] | null>(null);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [error, setError] = useState<boolean>(false);
-  const [dropdown, setDropdown] = useState("Enhancement Material");
-  const [region, setRegion] = useState("North America East");
+  const [data, setData] = useState<marketAPI[] | null>(null)
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [error, setError] = useState<boolean>(false)
+  const [dropdown, setDropdown] = useState("Enhancement Material")
+  const [region, setRegion] = useState("North America East")
   const [sorting, setSorting] = useState({
     key: "name",
     ascending: true,
-  });
+  })
 
   /* -------------------------------------------------------------------------- */
   /*                                  CONSTANTS                                 */
   /* -------------------------------------------------------------------------- */
-  const gold = "https://www.lostarkmarket.online/assets/icons/gold.png";
-  const marketURL = `https://www.lostarkmarket.online/api/export-market-live/${region}?category=${dropdown}`;
+  const gold = "https://www.lostarkmarket.online/assets/icons/gold.png"
+  const marketURL = `https://www.lostarkmarket.online/api/export-market-live/${region}?category=${dropdown}`
 
   /* -------------------------------------------------------------------------- */
   /*                             USEEFFECT: FETCHING                            */
   /* -------------------------------------------------------------------------- */
   useEffect(() => {
-    const controller = new AbortController();
+    const controller = new AbortController()
     const fetchAPI = async () => {
-      setError(false);
-      setIsLoading(true);
+      setError(false)
+      setIsLoading(true)
       try {
-        const data = await fetch(marketURL, { signal: controller.signal });
-        const jsonData = await data.json();
-        setData(jsonData);
+        const data = await fetch(marketURL, { signal: controller.signal })
+        const jsonData = await data.json()
+        setData(jsonData)
       } catch (error) {
-        controller.signal.aborted
-          ? console.log("Aborted the fetch")
-          : setError(true);
+        controller.signal.aborted ? console.log("Aborted the fetch") : setError(true)
       } finally {
-        setIsLoading(false);
+        setIsLoading(false)
       }
-    };
-    fetchAPI();
+    }
+    fetchAPI()
     return () => {
-      controller.abort();
-    };
-  }, [dropdown, region]);
+      controller.abort()
+    }
+  }, [dropdown, region])
 
   /* -------------------------------------------------------------------------- */
   /*                                  TABLESORT                                 */
   /* -------------------------------------------------------------------------- */
   useEffect(() => {
     if (data) {
-      const currentData = [...data];
+      const currentData = [...data]
 
       const sortedHeaders = currentData.sort((a, b) => {
-        let fa = a[sorting.key];
-        let fb = b[sorting.key];
+        let fa = a[sorting.key]
+        let fb = b[sorting.key]
 
         if (typeof fa === "string") {
           if (fa < fb) {
-            return -1;
+            return -1
           }
           if (fa > fb) {
-            return 1;
+            return 1
           }
-          return 0;
+          return 0
         }
         if (typeof fa === "number") {
-          return a[sorting.key] - b[sorting.key];
-        } else return 0;
-      });
+          return a[sorting.key] - b[sorting.key]
+        } else return 0
+      })
 
-      setData(sorting.ascending ? sortedHeaders : sortedHeaders.reverse());
+      setData(sorting.ascending ? sortedHeaders : sortedHeaders.reverse())
     }
-  }, [sorting.ascending, sorting.key]);
+  }, [sorting.ascending, sorting.key])
 
   function applySorting(key: any, ascending: any) {
     setSorting({
       key: key,
       ascending: ascending,
-    });
+    })
   }
 
   const sorted = (obj: any) =>
     Object.keys(obj)
       .sort()
       .reduce((accumulator: any, key: any) => {
-        accumulator[key] = obj[key];
+        accumulator[key] = obj[key]
 
-        return accumulator;
-      }, {});
+        return accumulator
+      }, {})
 
   /* -------------------------------------------------------------------------- */
   /*                              RETURN COMPONENT                              */
@@ -154,35 +152,33 @@ export const Market = () => {
           <thead className="cursor-pointer border-b-[1px] border-[#2B313A]">
             <tr className="">
               <th
-                className="min-w-[10rem] pb-3 pl-2 text-left md:min-w-[20rem]"
+                className="min-w-[10rem] pb-3 text-left  md:min-w-[20rem] md:px-3 "
                 onClick={() => applySorting("name", !sorting.ascending)}
               >
                 Name
               </th>
               <th
-                className="table-header pb-3 text-right"
+                className="table-header pb-3 text-right md:px-3"
                 onClick={() => applySorting("lowPrice", !sorting.ascending)}
               >
                 Lowest Price
               </th>
               <th
-                className="table-header pb-3 text-right"
+                className="table-header pb-3 text-right  md:px-3"
                 onClick={() => applySorting("recentPrice", !sorting.ascending)}
               >
                 Recent Price
               </th>
 
               <th
-                className="table-header pb-3 text-right"
+                className="table-header pb-3 text-right  md:px-3"
                 onClick={() => applySorting("lowPrice", !sorting.ascending)}
               >
                 Market Trend
               </th>
               <th
-                className="table-header pb-3 pr-2 text-right "
-                onClick={() =>
-                  applySorting("cheapestRemaining", !sorting.ascending)
-                }
+                className="table-header pb-3 pr-2 text-right  md:px-3 "
+                onClick={() => applySorting("cheapestRemaining", !sorting.ascending)}
               >
                 Quantity
               </th>
@@ -194,15 +190,7 @@ export const Market = () => {
           /* -------------------------------------------------------------------------- */}
           <tbody className="text-[0.825rem] tracking-tighter ">
             {data.map(
-              ({
-                name,
-                id,
-                image,
-                lowPrice,
-                cheapestRemaining,
-                recentPrice,
-                shortHistoric,
-              }) => {
+              ({ name, id, image, lowPrice, cheapestRemaining, recentPrice, shortHistoric }) => {
                 return (
                   <tr key={id} className="p-5 hover:bg-primary">
                     {/* Item name */}
@@ -253,12 +241,12 @@ export const Market = () => {
                       </span>
                     </TableRow>
                   </tr>
-                );
+                )
               }
             )}
           </tbody>
         </table>
       ) : null}
     </div>
-  );
-};
+  )
+}
